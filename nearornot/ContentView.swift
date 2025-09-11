f3404d2c4d693e77aa69038ab32c48c645010fdb
Taisuke Fukuno <fukuno@jig.jp>
2025-09-11 17:43:57 +0900

    Initial Commit

---
+++ nearornot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nearornot
+//
+//  Created by 福野　泰介 on 2025/09/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nearornot/nearornotApp.swift
@@ -0,0 +1,17 @@
+//
+//  nearornotApp.swift
+//  nearornot
+//
+//  Created by 福野　泰介 on 2025/09/11.
+//
+
+import SwiftUI
+
+@main
+struct nearornotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
