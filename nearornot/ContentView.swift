db18074963f6f054fd9cb469a63ddb7bf07304d9
Taisuke Fukuno <fukuno@jig.jp>
2025-09-12 16:14:41 +0900

    add near or not view

--- nearornot/ContentView.swift
+++ nearornot/ContentView.swift
@@ -1,24 +1,29 @@
-//
-//  ContentView.swift
-//  nearornot
-//
-//  Created by 福野　泰介 on 2025/09/11.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @State private var isNear = false
 
-#Preview {
-    ContentView()
+    var body: some View {
+        ZStack {
+            isNear ? Color.red : Color.green
+
+            Text(isNear ? "近い！" : "遠い")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+        }
+        .ignoresSafeArea()
+        .onAppear {
+            UIDevice.current.isProximityMonitoringEnabled = true
+            NotificationCenter.default.addObserver(
+                forName: UIDevice.proximityStateDidChangeNotification,
+                object: nil,
+                queue: .main
+            ) { _ in
+                isNear = UIDevice.current.proximityState
+            }
+        }
+        .onDisappear {
+            UIDevice.current.isProximityMonitoringEnabled = false
+        }
+    }
 }
